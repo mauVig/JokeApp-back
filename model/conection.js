5ec2974dcda603908d48270c058cd8014580f10c
loesen-maurovigliero <91024327+mauVig@users.noreply.github.com>
2022-07-29 20:07:07 -0300

    DELET INIT

--- model/conection.js
+++ model/conection.js
@@ -1,13 +1,14 @@
 import mongodb from 'mongodb'
 import dotenv from 'dotenv'
 dotenv.config()
+
 const client = new mongodb.MongoClient(process.env.MONGO_SER)
 
 
 export const conection = async (callbak) => {
   await client.connect();
 
-  const result = await callbak(client.db(proocess.env.DB_NAME));
+  const result = await callbak(client.db(process.env.DB_NAME));
 
   return result
 };
